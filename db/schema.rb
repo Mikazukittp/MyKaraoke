afa21299d1cc5cd76cdbfe7cc0792beda2b7bb52
matsumatsu20 <matsumatsu20@gmail.com>
2015-07-17 23:40:18 +0900

    Songモデルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717142736) do
+ActiveRecord::Schema.define(version: 20150717143747) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 20150717142736) do
     t.integer  "song_id",     null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "artist",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
