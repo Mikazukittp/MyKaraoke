acf2b93e8f880e88e554b0043671c430d1ecfbbd
matsumatsu20 <matsumatsu20@gmail.com>
2015-07-18 02:25:41 +0900

    show以外のCRUDを作成

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -8,13 +8,31 @@ class Api::V1::SongsController < ApplicationController
   def show
   end
 
+  # category_idsの配列を受け取ってそのカテゴリに紐づくSONGを作る
   def create
+    @song = Song.create(song_params)
+    @song.update(category_ids: params[:category_ids])
+
+    render json: @song
   end
 
   def update
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    @song.update(category_ids: params[:category_ids])
+
+    render json: @song
   end
 
   def destroy
+    Song.find(params[:id]).destroy!
+    render json: {message: '削除が完了しました'}, status: :ok
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :artist)
   end
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
-  has_many :category_songs
+  has_many :category_songs, dependent: :destroy
   has_many :categories, through: :category_songs
 
-  has_many :scores
+  has_many :scores, dependent: :destroy
 end
