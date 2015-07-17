2f80c42acfb89169d57d123c0355b418400bc3c3
matsumatsu20 <matsumatsu20@gmail.com>
2015-07-17 23:41:44 +0900

    UserTypeモデルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717143747) do
+ActiveRecord::Schema.define(version: 20150717144031) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 20150717143747) do
     t.string   "artist",     null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_types", force: :cascade do |t|
+    t.integer  "generation_id", null: false
+    t.integer  "gender_id",     null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
