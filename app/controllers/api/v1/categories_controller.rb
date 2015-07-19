a251714e601669627dd0bf898059378c92e2ae91
徳永 優作 <yusaku_tokunaga@r.recruit.co.jp>
2015-07-19 11:49:32 +0900

    一旦、cagoryies_controller作り途中だけどサーバーにあげたいのでpush

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,24 +1,35 @@
 class Api::V1::CategoriesController < ApplicationController
+  #一覧 > GET
+  #
   def index
     personal = {'name' => 'hoge', 'old' => 28}
     render :json => personal
   end
 
+  #ID指定して単体のCategoryを出す > GET
   def show
   end
 
+  #新規作成 > POST
   def create
+    @category = Category.create(category_params)
+    #ここには、将来的にUser
+
+    render json: @category
   end
 
+  #ID指定して編集する > PUT
   def update
   end
 
+  #ID指定して消去 > DELETE
   def destroy
   end
 
   private
 
-  def song_params
+  def category_params
+    params.require(:category).permit(:name, :description)
   end
 
 end
